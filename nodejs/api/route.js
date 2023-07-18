6c1a82f39f24264c10adee09b95331e13dd2b9e3
Atolycs <111569596+atolycs@users.noreply.github.com>
2023-07-18 12:11:01 +0900

    Add Path infomation

--- nodejs/api/route.js
+++ nodejs/api/route.js
@@ -1,5 +1,9 @@
 function _roots(fastify, options, done) {
     fastify.register(require('./meta-data/metadata'), { prefix: '/kvm'})
+    fastify.get('/kvm', (request, reply) => {
+        reply.send('meta-data/')
+    })
+    
     done()
 }
 
